0a59160d805b427f7aa10c98f6e9153795148140
isaac-holt <isaac_holt@icloud.com>
2022-07-11 22:42:16 +0100

    Change background color on overflow scroll

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,10 +11,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 				<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-				<link rel="manifest" href="/favicons/site.webmanifest" />
 				<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#2E79FF" />
 				<meta name="msapplication-TileColor" content="#2E79FF" />
-				<meta name="theme-color" content="#2E79FF" />
+				<link rel="manifest" href="/favicons/site.webmanifest" />
 			</Head>
 			<MenuBar />
 			<div className="mx-auto p-2 p-md-3 container-xxl">
