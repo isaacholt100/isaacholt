13e07fe0cf69d29a37c725c8cceed1870fb37132
isaac-holt <isaac_holt@icloud.com>
2022-07-11 18:24:42 +0100

    Added favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<div>
 			<Head>
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+				<link rel="manifest" href="/favicons/site.webmanifest" />
+				<link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#2E79FF" />
+				<meta name="msapplication-TileColor" content="#2E79FF" />
+				<meta name="theme-color" content="#2E79FF" />
 			</Head>
 			<MenuBar />
 			<div className="mx-auto p-2 p-md-3 container-xxl">
