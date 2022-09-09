b2aa614e8d52c9f1260a32700ba61173fca974b5
isaac-holt <isaac_holt@icloud.com>
2022-09-09 19:32:05 +0100

    Changed contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import pageTitle from "../lib/title";
 import Icon from "@mdi/react";
-import { mdiFacebook, mdiGithub, mdiGitlab, mdiInstagram, mdiLinkedin, mdiTwitter } from "@mdi/js";
+import { mdiEmail, mdiFacebook, mdiGithub, mdiGitlab, mdiInstagram, mdiLinkedin, mdiTwitter } from "@mdi/js";
 import PageTitle from "../components/PageTitle";
 import Link from "next/link";
 import Row from "react-bootstrap/Row";
@@ -17,8 +17,16 @@ interface Social {
 
 export const LINKEDIN_URL = "https://www.linkedin.com/in/isaacholt100/";
 
+const EMAIL = "isaacholt100@icloud.com";
+
 const SOCIALS: Social[] = [
-		{
+	{
+		name: "Email",
+		url: "mailto:" + EMAIL,
+		icon: mdiEmail,
+		color: "rgb(188, 74, 56)",
+	},
+	{
 		name: "LinkedIn",
 		url: LINKEDIN_URL,
 		icon: mdiLinkedin,
@@ -35,34 +43,13 @@ const SOCIALS: Social[] = [
 		url: "https://www.gitlab.com/isaacholt100/",
 		icon: mdiGitlab,
 		color: "rgb(224, 103, 42)",
-	},
-	{
-		name: "Instagram",
-		url: "https://www.instagram.com/isaacholt100/",
-		icon: mdiInstagram,
-		color: "rgb(199, 47, 63)",
-	},
-	{
-		name: "Facebook",
-		url: "https://www.facebook.com/profile.php?id=100073407960215",
-		icon: mdiFacebook,
-		color: "rgb(48, 94, 222)",
-	},
-	{
-		name: "Twitter",
-		url: "https://twitter.com/isaacholt100/",
-		icon: mdiTwitter,
-		color: "rgb(61, 127, 223)",
 	}
 ];
-
-const EMAIL = "isaacholt100@icloud.com";
 
 export default function Contact() {
 	return (
 		<>
 			<PageTitle title="Contact" />
-			<p>To get in touch, email me at <a href={"mailto:" + EMAIL}>{EMAIL}</a> or connect with me on these platforms:</p>
 			<Row className="g-2 g-md-3">
 				{SOCIALS.map(social => (
 					<Col xs={12} sm={6} xl={4} key={social.name}>
