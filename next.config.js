af018d509cb1a5235588dc8a2da14c29cb71513a
Isaac Holt <isaac_holt@icloud.com>
2023-04-14 16:23:06 +0100

    Update next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = async (phase, ...args) => ({
   reactStrictMode: console.log(args) ? true : true,
   output: process.env.HOST === "vercel" ? undefined : "export",
   images: {
-	unoptimized: true,
+	unoptimized: process.env.HOST === "vercel" ? false : true,
   }
 });
 
